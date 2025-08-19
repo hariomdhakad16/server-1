58fbb31a80a336cab1801837481fe78f8f0286bf
DhakadZone <mrvikashkirar478@gmail.com>
2025-08-19 20:41:56 +0530

    hi

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-//const LiveUrl = "mongodb+srv://mrvikashkirar:Dhakadsahab27@cluster0.zohbwxj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 const connectDB = async ()=>{
     return mongoose.connect(process.env.LiveUrl)
